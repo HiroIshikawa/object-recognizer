cb45556eb52009e39e22c4dec43b62c8675f744b
HiroIshikawa <hiro52ishikawa@gmail.com>
2016-12-19 20:07:36 -0800

    Add rotated cascade generator setting

--- model/cascade/cascade_tester/testCascadeRealTime.py
+++ model/cascade/cascade_tester/testCascadeRealTime.py
@@ -2,10 +2,18 @@
 #using detectMultiScale
 import numpy
 import cv2
+from sys import argv
+
+obj_w = int(argv[1])
+obj_h = int(argv[2])
+scale_factor = float(argv[3])
+min_neighs = int(argv[4])
+win_w = int(argv[5])
+win_h = int(argv[6])
 
 def detect(img):
     cascade = cv2.CascadeClassifier("cascade.xml")
-    rects = cascade.detectMultiScale(img, 1.3, 4, cv2.cv.CV_HAAR_SCALE_IMAGE, (30,60))
+    rects = cascade.detectMultiScale(img, scale_factor, min_neighs, cv2.cv.CV_HAAR_SCALE_IMAGE, (obj_w,obj_h))
 
     if len(rects) == 0:
         return [], img
@@ -15,11 +23,11 @@ def detect(img):
 def box(rects, img):
     for x1, y1, x2, y2 in rects:
         cv2.rectangle(img, (x1, y1), (x2, y2), (127, 255, 0), 2)
-    cv2.imwrite('one.jpg', img);
+    # cv2.imwrite('one.jpg', img);
 
 cap = cv2.VideoCapture(0)
-cap.set(3,400)
-cap.set(4,300)
+cap.set(3,win_w)
+cap.set(4,win_h)
 
 while(True):
     ret, img = cap.read()
