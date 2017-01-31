f43585b18260fa76e7b3bb8a9b575c2da03bdef0
HiroIshikawa <hiro52ishikawa@gmail.com>
2017-01-30 20:09:07 -0800

    Add multi-cascades detector

--- processor/detect_realtime/detect_realtime.py
+++ processor/detect_realtime/detect_realtime.py
@@ -2,9 +2,13 @@
 #using detectMultiScale
 from sys import argv
 import cv2
+import glob
+import itertools
 import numpy as np
 import time
 import threading
+# import xml
+import xml.etree.ElementTree as ET
 
 obj_w = int(argv[1])
 obj_h = int(argv[2])
@@ -12,6 +16,7 @@ scale_factor = float(argv[3])
 min_neighs = int(argv[4])
 win_w = int(argv[5])
 win_h = int(argv[6])
+
 
 def region_of_interest(img, vertices):
     """
@@ -38,7 +43,16 @@ def region_of_interest(img, vertices):
     masked_image = cv2.bitwise_and(img, mask)
     return masked_image
 
-def detect(img):
+def initiate_frame(cap):
+    ret, img = cap.read()
+    imshape = img.shape
+    vertices = np.array([[(0,imshape[0]),(0,int(imshape[0]/2)), 
+        (int(imshape[1]),int(imshape[0]/2)), (imshape[1],imshape[0])]], dtype=np.int32)
+    cv2.imshow("frame1", img)
+    img = region_of_interest(img, vertices)
+    return ret, img 
+
+# def detect_object(img):
     """
     Detects objects that matches with cascade classifiers.
 
@@ -46,26 +60,34 @@ def detect(img):
     rectangles. Each rect data contains: (x1, y1, x2, y2)
     """
 # The version for the multiple cascades
-# def detect(cascade_list, img):
-    # # Read width and height of a cascade from cascade.xml
+def detect_object(img):
+    # Read width and height of a cascade from cascade.xml
     # for cascade in enumarate(cascade_list):
-    # for cascade in enumerate(glob.iglob('./cascades/*.xml')):
-    #   root = xml.etree.ElementTree.parse(cascade).getroot()
-    #   width = root[0][3].text
-    #   height = root[0][2].text
-    #   cur_rects = cascade.detectMultiScale(img, 1.1, 1, cv2.cv.CV_HAAR_SCALE_IMAGE, (width,height))
-    #   for rect in cur_rects:
-    #       total_rects.append(rect)
-    #   total_rects.append(cur_rects[0])
-    cascade = cv2.CascadeClassifier("cascade.xml")
-    rects = cascade.detectMultiScale(img, scale_factor, min_neighs, cv2.cv.CV_HAAR_SCALE_IMAGE, (obj_w,obj_h))
-
-    if len(rects) == 0:
+    rects_list = []
+    for cascade in glob.iglob('./cascades/*.xml'):
+        root = ET.parse(cascade).getroot()
+        width = int(root[0][3].text)
+        height = int(root[0][2].text)
+        casc = cv2.CascadeClassifier(cascade)
+        cur_rects = casc.detectMultiScale(img, 1.1, 1, cv2.cv.CV_HAAR_SCALE_IMAGE, (width,height))
+        rects_list.append(cur_rects)
+    if isinstance(rects_list[0], tuple):
         return [], img
+    rects = list(itertools.chain(rects_list))[0]
+    
+    """Sincgle casacde version"""
+    # # cascade = cv2.CascadeClassifier("cascade.xml")
+    # # rects = cascade.detectMultiScale(img, scale_factor, min_neighs, cv2.cv.CV_HAAR_SCALE_IMAGE, (obj_w,obj_h))
+    # # ## return tuple when nothing inside, otherwise 2d array
+    # # if type(rects)=='tuple':
+    # #     print(rects.type)
+    # # if len(rects) == 0:
+    #     return [], img
     rects[:, 2:] += rects[:, :2]
+    
     return rects, img
 
-def box(rects, img):
+def draw_box(rects, img):
     """
     Draws box around the detected objects.
 
@@ -122,46 +144,7 @@ def pos_from_center(img_center, box_center):
     # print('position of box to y: '+str(box_rel_y))
     return (box_rel_x, box_rel_y)
 
-def mean(l):
-    if len(l)==0:
-        return 0.0
-    return sum(l)/len(l)
-
-def accm_position(candidates):
-    threading.Timer(0.5, accm_position, args=[candidates]).start()
-    # avg_pos = reduce(lambda x, y: x + y, candidates) / len(candidates)
-    avg_pos = mean(candidates)
-    candidates[:] = []
-    if (avg_pos > 0):
-        print("Turn Right: Rotate "+str(avg_pos)+" units")
-    elif (avg_pos < 0):
-        print("Turn Left: Rorate "+str(avg_pos)+" units")
-    else:
-        print("Go Straight")
-    # print("Navigation: "+str(avg_pos)+" units")
-    # return True
-
-cap = cv2.VideoCapture(0)
-cap.set(3,win_w)
-cap.set(4,win_h)
-
-candidates = []
-
-accm_position(candidates)
-
-while(True):
-    ret, img = cap.read()
-    imshape = img.shape
-    vertices = np.array([[(0,imshape[0]),(0,int(imshape[0]/2)), 
-        (int(imshape[1]),int(imshape[0]/2)), (imshape[1],imshape[0])]], dtype=np.int32)
-    cv2.imshow("frame1", img)
-    img = region_of_interest(img, vertices)
-
-    rects, img = detect(img)
-    box(rects, img)
-
-    flag = False
-
+def measure_position(img, rects, candidates):
     # Get the coordinate of the center of the image
     img_center = centering_img(img)
 
@@ -184,26 +167,78 @@ while(True):
         box_to_center = pos_from_center(img_center, box_center)
         boxes[i]['box_to_center'] = box_to_center
 
-    # for box in boxes:
-    #     print(box)
     # for each box, find the best one to approach 
     if (boxes):
-        # elminate the boxes that above the horizon
-
-
         # find the box having the maximum size
         maxSizeItem = max(boxes, key=lambda x:x['box_size'][0]*x['box_size'][1])
-
-        # if (maxSizeItem['box_to_center'][0] > 0):
-        #     # print("Turn Right: Rotate "+str(maxSizeItem['box_to_center'][0])+" units")
-        # elif (maxSizeItem['box_to_center'][0] < 0):
-        #     # print("Turn Left: Rorate "+str(maxSizeItem['box_to_center'][0])+" units")
-        # else:
-        #     # print("Go Straight")
-        
-        # print("The item which has max size: "+str(maxSizeItem))
-
         candidates.append(maxSizeItem['box_to_center'][0])
+    return candidates
+
+def command(avg_pos):
+    if (avg_pos > 10):
+        print("Turn Right: Rotate "+str(avg_pos)+" units")
+#        var = 6
+#        writeNumber(var)
+#        print "RPI: Hi Arduino, I sent you", var
+    elif (avg_pos < -10):
+        print("Turn Left: Rorate "+str(avg_pos)+" units")
+#        # left q = 5
+#        var = 5
+#        writeNumber(var)
+#        print "RPI: Hi Arduino, I sent you", var
+    else:
+        print("Go Straight")
+#        var = 1
+#        writeNumber(var)
+#        print "RPI: Hi Arduino, I sent you", var
+#    print('')
+
+def mean(l):
+    if len(l)==0:
+        return 0.0
+    return sum(l)/len(l)
+
+def navigate(candidates):
+    threading.Timer(0.5, navigate, args=[candidates]).start()
+    # avg_pos = reduce(lambda x, y: x + y, candidates) / len(candidates)
+    global command_flag
+    global avg_pos
+    if candidates:
+        # command(candidates)
+        avg_pos = mean(candidates)
+        # print("Candi found: "+str(avg_pos))
+        command_flag = True
+        # print(command_flag)
+        candidates[:] = []
+    else:
+        # command_flag = False
+        pass
+
+
+cap = cv2.VideoCapture(0)
+cap.set(3,win_w)
+cap.set(4,win_h)
+
+global command_flag
+global avg_pos
+command_flag = False
+avg_pos = 0.
+candidates = []
+navigate(candidates)
+
+# accm_position(candidates)
+
+while(True):
+    ret, img = initiate_frame(cap)
+    rects, img = detect_object(img)
+    draw_box(rects, img)
+    candidates = measure_position(img, rects, candidates)
+
+    if command_flag:
+        # print("command activated")
+        command(avg_pos)
+        command_flag = False
+        # time.sleep(1)
 
     cv2.imshow("frame", img)
     if(cv2.waitKey(1) & 0xFF == ord('q')):
