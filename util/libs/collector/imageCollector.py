0af0e570275ded34bd8f34697bdef3d78d8d2000
HiroIshikawa <hiro52ishikawa@gmail.com>
2017-01-31 12:33:54 -0800

    Image collector refactored

--- util/libs/collector/imageCollector.py
+++ util/libs/collector/imageCollector.py
@@ -1,19 +1,22 @@
 # open arbitrary resources (https://docs.python.org/2/library/urllib.html)
+import socket
 import urllib2
 
 # urlsFile = 'cokeURL.txt'
 urlsFile = 'cansURL.txt'
 # reading lines of txt file (URLs)
-urls = tuple(open(urlsFile, 'r'))
+urls = open(urlsFile, 'r')
 # id number for writing images
 numbering = 0
 # stop iterations to downloads
-iterations = 100
+iterations = 300
 
+# url_lines = urls.readlines()
 # iterate the txt file contains urls line by line
-for url in urls.split('\n'):
+for url in urls:
+	print(url)
 	# notify which iteration you are in
-	print iterations
+	print(iterations)
 	# make a string for a path name to write the file in
 	writePath = 'downloads/' + str(numbering) + '.jpg'
 	# open a file to write in with the writePath name defined
@@ -22,9 +25,12 @@ for url in urls.split('\n'):
 		# retrieve resource with the url defined
 		response = urllib2.urlopen(url, timeout=0.1)
 	except urllib2.URLError as err:
-		print 'URL error happend'
+		print('URL error happend')
 		pass
 	except IOError:
+		print('IO Error happend')
+		pass
+	except socket.timeout:
 		pass
 	else:
 		# print response
@@ -32,13 +38,17 @@ for url in urls.split('\n'):
 		# if image is loaded successfully
 		if response:
 			# read the image
-			img = response.read()
-			# write actually the fetched image in the file location
-			f.write(img)
-			# increment image numbering
-			numbering += 1
-			# increment counts towards topper
-			iterations -= 1
+			try:
+				img = response.read()
+			except socket.timeout:
+				pass
+			else:
+				# write actually the fetched image in the file location
+				f.write(img)
+				# increment image numbering
+				numbering += 1
+				# increment counts towards topper
+				iterations -= 1
 		
 		# if k images downloaded, stop iteration
 	if (iterations <= 0):
