Fix black edge issues on image rotation

util/bin/gray_resize_rotate/gray_resize_rotate.py
@@ -1,49 +1,14 @@
-import glob
-import cv2
-import shutil
-import os
-import numpy as np
+from PIL import Image
 from sys import argv
-
-w = int(argv[1])
-h = int(argv[2])
-is_rorate = argv[3]
-is_parallel = argv[4]
-
-shutil.rmtree('./output')
-os.system('mkdir output')
-
-def rotate_bound(image, angle):
-
-    (h, w) = image.shape[:2]
-    (cX, cY) = (w // 2, h // 2)
- 
-    M = cv2.getRotationMatrix2D((cX, cY), -angle, 1.0)
-    cos = np.abs(M[0, 0])
-    sin = np.abs(M[0, 1])
- 
-    nW = int((h * sin) + (w * cos))
-    nH = int((h * cos) + (w * sin))
- 
-    M[0, 2] += (nW / 2) - cX
-    M[1, 2] += (nH / 2) - cY
- 
-    return cv2.warpAffine(image, M, (nW, nH))
-
-def rotate(img,angle):
-    rows,cols = img.shape
-
-    M = cv2.getRotationMatrix2D((cols/2,rows/2),angle,1)
-    dst = cv2.warpAffine(img,M,(cols,rows))
-
-    return dst
+import cv2
+import glob
+import numpy as np
+import os
+import shutil
 
 def process(filename, key, w, h):
-
-    image = cv2.imread(filename)
-
-    gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    gray_resized_image = cv2.resize(gray_image, (w, h))
+    image = Image.open(filename)
+    resized_image = image.resize((w,h))
     if is_rorate=='y':
         if is_parallel=='y':
             if i%2==0:
@@ -57,12 +22,18 @@ def process(filename, key, w, h):
                 angle = 180
     else:
         angle = 0
-    # gray_resized_rotated_image = rotate_bound(gray_resized_image, angle)
-    gray_resized_rotated_image = rotate(gray_resized_image, angle)
-    print("writing..")
-    print(gray_resized_rotated_image)
-    print(filename)
-    cv2.imwrite('./output/'+str(key)+'.jpg', gray_resized_rotated_image)
+    resized_rotated_image = resized_image.rotate(angle)
+    resized_rotated_gray_image = cv2.cvtColor(np.array(resized_rotated_image), cv2.COLOR_RGB2GRAY)
+    print(filename + ' converted')
+    cv2.imwrite('./output/'+str(key)+'.jpg', resized_rotated_gray_image)
+
+w = int(argv[1])
+h = int(argv[2])
+is_rorate = argv[3]
+is_parallel = argv[4]
+
+shutil.rmtree('./output')
+os.system('mkdir output')
 
 for (i,image_file) in enumerate(glob.iglob('./source/*.jpg')):
 	process(image_file, i, w, h)